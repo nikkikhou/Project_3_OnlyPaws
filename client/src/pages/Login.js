7fe2b862a9a1f7b01b7a06287f208eb3a282ad0c
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-13 18:54:22 -0500

    debugged login and loggout but had to restyle page.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -45,26 +45,56 @@ const Login = (props) => {
       {data ? (
         <p>Successfully logged in! You may now head{' '}<Link to='/'>back to the hompage.</Link></p>
       ) : (
-        <div>
-          <h2>Login</h2>
-          <form onSubmit={handleFormSubmit}>
-            <input
-              placeholder='Your email'
+
+
+<section className="hero is-dark is-fullheight">
+  <div className="hero-body">
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-2-tablet ">
+          <form onSubmit={handleFormSubmit} className="box login-form">
+            <div className="field">
+              <label for="" className="label">Email</label>
+              <div className="control has-icons-left">
+              <input
+              placeholder='Email'
               name='email'
               type='email'
               value={formState.email}
               onChange={handleChange}
-            />
-            <input
-              placeholde='Your password'
+            />               
+             <span className="icon is-small is-left">
+                  <i className="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+              </div>
+            </div>
+            <div className="field">
+              <label for="" class="label">Password</label>
+              <div className="control has-icons-left">
+              <input
+              placeholde='Password'
               name='password'
               type='password'
               value={formState.password}
               onChange={handleChange}
-            />
-            <button type='submit'>Login</button>
+            />                
+            <span className="icon is-small is-left">
+                  <i className="fa fa-lock"></i>
+                </span>
+              </div>
+            </div>
+            <div className="field">
+              <button className="button is-info">
+                Login
+              </button>
+            </div>
           </form>
         </div>
+      </div>
+    </div>
+  </div>
+</section>
+
       )}
       {error && (
         <div>{error.message}</div>
