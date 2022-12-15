f41350074b2aab085244cb3082aff1bde82138d8
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-12-15 10:53:01 -0500

    added bulma styling to landing, login, sign up

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,21 +3,53 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <main className='landing-page'>
-      <div className="tile landing-tile-container is-ancestor has-text-centered ">
-        <div className="tile landing-tile is-4 is-vertical is-parent">
-          <div className="tile is-child box">
-            <p className="title">Only Paws</p>
-            <Link to='/signup' style={{ textDecoration: 'none' }}>
-              <h1 className='landing-h1'>Sign Up now</h1>
-            </Link>
-            <Link to='/login' style={{ textDecoration: 'none' }}>
-              <h1 className='landing-h1'>Login now</h1>
-            </Link> 
+    // 
+    <body>
+      <section class="hero is-info is-fullheight">
+        <div class="hero-head">
+          <nav class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <span class="navbar-burger burger" data-target="navbarMenu">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navbarMenu" class="navbar-menu">
+                <div class="navbar-end">
+                  <span class="navbar-item">
+                  <Link class="is-size-3 has-text-centered" to='/login' style={{ textDecoration: 'none' }}>
+        <h2>Login</h2>
+      </Link>
+                  </span>
+                  <span class="navbar-item">
+                  <Link class="is-size-3 has-text-centered" to='/signup' style={{ textDecoration: 'none' }}>
+        <h2>Sign Up</h2>
+      </Link>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <div class="column is-6 is-offset-3">
+              <h1 class="title">
+                Welcome to OnlyPaws!!!
+              </h1>
+              <h2 class="subtitle">
+                This is the a social media platform to all of our four legged friends out there! For those of you new to the litter.... Welcome!
+              </h2>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+
+      </section>
+      <script async type="text/javascript" src="../js/bulma.js"></script>
+    </body>
   )
 };
 
