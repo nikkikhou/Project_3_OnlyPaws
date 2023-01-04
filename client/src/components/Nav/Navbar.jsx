7d398f290a0884dffc260d9f8f2fca3a8bd8edec
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-03 19:46:34 -0500

    spaced login and sign up links.

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -5,19 +5,6 @@ import { Link } from "react-router-dom";
 function Navbar({ currentPage, handlePageChange }) {
 
     return (
-    // <nav class="navbar is-info" role="navigation" aria-label="main navigation">
-    //   <div class="navbar-brand">
-    //     <a class="navbar-item">
-    //       <img src="https://images.cooltext.com/5634882.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="150" height="130"></img>
-    //     </a>
-    //     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-    //       <span aria-hidden="true"></span>
-    //       <span aria-hidden="true"></span>
-    //       <span aria-hidden="true"></span>
-    //     </a>
-    //   </div>
-    // </nav>
-
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <p class="navbar-item">
@@ -32,12 +19,12 @@ function Navbar({ currentPage, handlePageChange }) {
 
   <div class="navbar-end">
     <div class="navbar-item">
-      <div class="">
+      <div class="signup-links">
         <Link class="has-text-centered" to='/signup' style={{ textDecoration: 'none' }}>
-          <p>Sign Up</p>
+          <p className='sign-up'>Sign Up</p>
         </Link>
         <Link class="has-text-centered" to='/login' style={{ textDecoration: 'none' }}>
-          <p>Login</p>
+          <p className='login'>Login</p>
         </Link>
       </div>
     </div>
