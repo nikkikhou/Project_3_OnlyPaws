fa76e953a04e97d667b3414a68e7c3a58048ab49
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2023-01-05 13:31:06 -0500

    contiuing styinling home page - added some styling to home page title.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       </div>
       <div id="home-container" class="tile is-ancestor">
         <div class="tile is-3 is-vertical is-parent">
-          <div class="tile is-child box">
+          <div class="banner tile is-child box">
             <p class="title">One</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
           </div>
